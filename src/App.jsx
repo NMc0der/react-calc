92530777092c862ca11b35ce21b32c71bf559d33
Noah <noahglennmaldonado@gmail.com>
2023-12-04 02:36:43 -0500

    organized layout thought html and css

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,69 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  const genButtons = (num) => {
-    const numbers = [];
-
-    for (let i = 0; i < num; i++) {
-      numbers.push(<button>{i}</button>);
-    }
-    return numbers;
-  };
-
   return (
     <div className="calc-grid">
-      <div className="display"></div>
-      <div className="buttons">
-        <div className="display-btns">
-          <button>AC</button>
-          <button>%</button>
-          <button>+/-</button>
-        </div>
-        <div className="operation-btns">
-          <button>/</button>
-          <button>X</button>
-          <button>+</button>
-          <button>-</button>
-          <button>=</button>
-        </div>
-        <div className="number-btns">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>0</button>
-          <button>.</button>
-        </div>
+      <div className="wrapper">
+        <div className="display"></div>
+        <button className="btn" onClick={reset}>
+          AC
+        </button>
+        <button className="btn" onClick={precent}>
+          %
+        </button>
+        <button className="btn" onClick={minusPlus}>
+          +/-
+        </button>
+
+        <button className="btn" onClick={operatorType}>
+          /
+        </button>
+
+        <button className="btn" onClick={inputNum}>
+          7
+        </button>
+        <button className="btn" onClick={inputNum}>
+          8
+        </button>
+        <button className="btn" onClick={inputNum}>
+          9
+        </button>
+        <button className="btn" onClick={operatorType}>
+          X
+        </button>
+        <button className="btn" onClick={inputNum}>
+          4
+        </button>
+        <button className="btn" onClick={inputNum}>
+          5
+        </button>
+        <button className="btn" onClick={inputNum}>
+          6
+        </button>
+        <button className="btn" onClick={operatorType}>
+          +
+        </button>
+        <button className="btn" onClick={inputNum}>
+          1
+        </button>
+        <button className="btn" onClick={inputNum}>
+          2
+        </button>
+        <button className="btn" onClick={inputNum}>
+          3
+        </button>
+        <button className="btn" onClick={operatorType}>
+          -
+        </button>
+        <button className="btn" onClick={inputNum}>
+          0
+        </button>
+        <button className="btn" onClick={inputNum}>
+          .
+        </button>
+        <button className="btn" onClick={equals}>
+          =
+        </button>
       </div>
     </div>
   );
